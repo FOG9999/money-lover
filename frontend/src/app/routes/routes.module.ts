16c779e3e030dfbdc7a3080f3e11d5356ab30258
fog9999 <fog9999>
2023-04-09 14:25:07 +0700

    remove unused code

--- frontend/src/app/routes/routes.module.ts
+++ frontend/src/app/routes/routes.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from '@shared/shared.module';
 import { RoutesRoutingModule } from './routes-routing.module';
-
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './sessions/login/login.component';
 import { RegisterComponent } from './sessions/register/register.component';
 
-const COMPONENTS = [DashboardComponent, LoginComponent, RegisterComponent];
+const COMPONENTS = [LoginComponent, RegisterComponent];
 const COMPONENTS_DYNAMIC = [];
 
 @NgModule({
