373cbc8b514daf1cb948e794bb0046a40b39bd67
fog9999 <fog9999>
2023-04-30 07:26:22 +0700

    api for report

--- backend/config/consts.js
+++ backend/config/consts.js
@@ -71,5 +71,21 @@ module.exports = {
 
     enableCORS: true,
     page_size: 10,
-    icon_type: '.png'
+    icon_type: '.png',
+    type_income: 1,
+    type_outcome: 0,
+    months: [
+        "Tháng 1",
+        "Tháng 2",
+        "Tháng 3",
+        "Tháng 4",
+        "Tháng 5",
+        "Tháng 6",
+        "Tháng 7",
+        "Tháng 8",
+        "Tháng 9",
+        "Tháng 10",
+        "Tháng 11",
+        "Tháng 12",
+    ]
 }
