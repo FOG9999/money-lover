e9912c81d1c7b6af32cdc42e4b8b7845f7086eca
FOG9999 <nguyenducthang20081999@gmail.com>
2023-10-15 14:53:09 +0700

    add config for stage

--- frontend/src/environments/environment.stage.ts
+++ frontend/src/environments/environment.stage.ts
@@ -1,7 +1,7 @@
 export const environment = {
     PassportLoginServerURL: "https://59u9issq74.execute-api.us-east-1.amazonaws.com/api",
-    MoneyLoverURL: "https://my-ml.net",
-    SERVER_URL: 'https://gzo62gz3a0.execute-api.us-east-1.amazonaws.com/api/rest',
+    MoneyLoverURL: "https://my-ml.xyz",
+    SERVER_URL: 'https://s7k48hs59c.execute-api.us-east-1.amazonaws.com/api/rest',
     production: false,
     useHash: false,
     hmr: false,
